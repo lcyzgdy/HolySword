997a126a3ea98b05f0ba60e301ba4b575d501eb2
lcyzgdy <MyGuanDY@outlook.com>
2017-09-14 22:27:54 +0800

    Linear space

---
+++ Assets/Script/AttackBehaviour/GongJi01_Idle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GongJi01_Idle : StateMachineBehaviour
+{
+	public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+	{
+
+	}
+
+	public override void OnStateIK(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+	{
+		Debug.Log("A");
+	}
+}
